986ff4f3576d7587047ebcddc974bcfc775f6dbd
Žiga Pregelj <ziga.pregelj@razum.si>
2023-12-08 23:35:18 +0100

    day 8 part 2 done

--- python/Day 8/second_part.py
+++ python/Day 8/second_part.py
@@ -47,6 +47,16 @@ def get_ending_points(path_dict):
 
 # ['NBA', 'SXA', 'JVA', 'XVA', 'AAA', 'GRA']
 # ['PSZ', 'VTZ', 'ZZZ', 'VGZ', 'BVZ', 'BPZ']
+
+# "NBA" : "BVZ" - 13939
+# "SXA" : "VGZ" - 17621
+# "JVA" : "VTZ" - 11309
+# "XVA" : "BPZ" - 20777
+# "AAA" : "ZZZ" - 19199
+# "GRA" : "PSZ" - 15517
+
+
+# do nort work as expected
 def walk(path, directions):
     steps = 0
     start1 = "GRA"
@@ -55,20 +65,18 @@ def walk(path, directions):
     start4 = "JVA"
     start5 = "SXA"
     start6 = "NBA"
-    finish = ['PSZ', 'VTZ', 'ZZZ', 'VGZ', 'BVZ', 'BPZ']
+
+    finish1 = "PSZ"
+    finish2 = "ZZZ"
+    finish3 = "BPZ"
+    finish4 = "VTZ"
+    finish5 = "VGZ"
+    finish6 = "BVZ"
+
     while True:
         for direction in directions:
-            starting_points = set()
-            starting_points.add(start1)
-            starting_points.add(start2)
-            starting_points.add(start3)
-            starting_points.add(start4)
-            starting_points.add(start5)
-            starting_points.add(start6)
-            # print(starting_points)
-            # print(finish)
-            # print("presek", set(starting_points) & set(finish))
-            if len(set(starting_points) | set(finish)) == 6:
+
+            if start1 == finish1 and start2 == finish2 and start3 == finish3 and start4 == finish4 and start5 == finish5 and start6 == finish6:
                 return steps
             else:
                 start1 = path[start1][direction]
@@ -81,8 +89,26 @@ def walk(path, directions):
                 steps += 1
 
 
-def finish_simultaneously():
-    ...
+def walk_2(path, directions, start, finish):
+    steps = 0
+    count_while_loop = 0
+    while start != finish:
+        for direction in directions:
+            if start == finish:
+                return steps
+            else:
+                start = path[start][direction]
+                steps += 1
+        count_while_loop += 1
+    return steps
+
+
+def finish_simultaneously(walk_path, directions, starting_points, ending_points):
+    all_possible = defaultdict(tuple)
+    for starting_point in starting_points:
+        for ending_point in ending_points:
+            all_possible[starting_point] = (ending_point, walk_2(walk_path, directions, starting_point, ending_point))
+    return all_possible
 
 
 indexed_directions = (convert_left_right_to_index(left_right(b)))
@@ -92,7 +118,27 @@ starting_points = get_starting_points(path)
 ending_points = get_ending_points(path)
 # print(starting_points)
 # print(ending_points)
-print(walk(path, indexed_directions))
+#print(walk(path, indexed_directions))
 # print(len(set(starting_points) | set(ending_points)) )
+#finish_simultaneously(path, indexed_directions, starting_points, ending_points)
+
+# "NBA" : "BVZ" - 13939
+# "SXA" : "VGZ" - 17621
+# "JVA" : "VTZ" - 11309
+# "XVA" : "BPZ" - 20777
+# "AAA" : "ZZZ" - 19199
+# "GRA" : "PSZ" - 15517
+# ['PSZ', 'VTZ', 'ZZZ', 'VGZ', 'BVZ', 'BPZ']
+# ['PSZ', 'VTZ', 'ZZZ', 'VGZ', 'BVZ', 'BPZ']
+
 
 # too low 11309
+
+steps = [13939, 17621, 11309, 20777, 19199, 15517]
+
+from math import gcd
+#will work for an int array of any length
+lcm = 1
+for i in steps:
+    lcm = lcm*i//gcd(lcm, i)
+print(lcm)
